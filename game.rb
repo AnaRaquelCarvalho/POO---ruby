9e52d9d11d8b342b2182db244d188975290eab2d
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2025-09-04 21:41:23 -0300

    Update game.rb

---
+++ game.rb
@@ -0,0 +1,16 @@
+puts '-' * 60
+puts 'POO - CRIANDO A 1ª CLASSE E INSTÂNCIA'.center(60)
+puts '-' * 60
+
+class Game
+end
+
+pes = Game.new
+resident_evil = Game.new
+zelda = Game.new
+
+puts pes, resident_evil, zelda
+puts pes.class, resident_evil.class, zelda.class
+
+puts '-' * 60
+
