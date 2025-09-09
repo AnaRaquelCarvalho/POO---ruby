281e53753821f4609464f6439eebd0a5e8ce3729
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2025-09-08 21:25:49 -0300

    .

--- Polimorfismo.rb
+++ Polimorfismo.rb
@@ -1,8 +1,7 @@
 puts'-' * 60
-puts' UTILIZANDO HERANÇA '.center(60)
+puts ' UTILIZANDO POLIMORFISMO '.center(60)
 puts'-' * 60
 
-# Classe base Game
 class Game
     attr_accessor :name, :genre, :price # Leitura e Escrita
     attr_reader :multiplayer, :versions
@@ -16,12 +15,10 @@ class Game
         @price = price
     end
     
-    # Método de conversão para string (imprime resumo do objeto)
     def to_s
         "#{@name}-#{@genre}-#{@multiplayer}-#{@versions}"
     end
 
- 
     def techinal_sheet
         puts "##Dados do Jogo##"
         puts "Nome do Jogo: #{@name}"
@@ -30,23 +27,24 @@ class Game
     end
 end
 
-# Classe filha DownloadableGame (herda de Game)
 class DownloadableGame < Game
-    attr_accessor :download_size            # Tamanho do download (em GB, por exemplo)
+    attr_accessor :download_size
 
-    # Construtor da classe DownloadableGame
     def initialize(name, genre, multiplayer, versions, price, download_size)
         super(name, genre, multiplayer, versions, price)
         @download_size = download_size
     end
+
+    def techinal_sheet
+        super
+        puts "Tamanho do Download: #{@download_size} GB"
+    end
 end
 
-# Criação de um objeto da classe Game
 game1 = Game.new("Fifa", "Esporte", true, [2018, 2019, 2020], 100)
 puts game1
 puts game1.techinal_sheet
 
-# Criação de um objeto da classe DownloadableGame
 game2 = DownloadableGame.new("Call of Duty", "Ação", true, ["Black Ops"], 100, 60)
 puts game2
 puts game2.techinal_sheet
